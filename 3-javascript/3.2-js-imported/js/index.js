3bd26e0adc60fab1f293c40106dab6fd40ea50d1
IvanGM <ivangm_01@hotmail.com>
2023-04-17 21:32:29 -0600

    variables agregadas

--- 3-javascript/3.2-js-imported/js/index.js
+++ 3-javascript/3.2-js-imported/js/index.js
@@ -1,3 +1,8 @@
+//mostrar un mensaje en la consola
 console.log("Hola mundo");
+/*
+las siguientes son para definir e inicializar una variable
+para mostrar en consola
+*/
 var fatherName = "jose";
 console.log(fatherName);
