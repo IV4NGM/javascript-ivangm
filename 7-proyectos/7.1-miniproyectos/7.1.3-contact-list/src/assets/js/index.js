2b45b107da34228b88382e8cbcec9365db08b2a8
IvanGM <ivangm_01@hotmail.com>
2023-05-08 21:37:54 -0600

    feat: tarjeta de contactos añadida

---
+++ 7-proyectos/7.1-miniproyectos/7.1.3-contact-list/src/assets/js/index.js
@@ -0,0 +1,63 @@
+// const ulElement = document.getElementById("contacts-ul");
+const ulElement = document.querySelector('#contacts-ul');
+let contacts = [];
+const searchingInput = document.querySelector('#studentInput');
+
+function renderContacts(contactsArray) {
+    let i = 0;
+    while(i<contactsArray.length){
+        const li = document.createElement('li');
+        const cardDiv = document.createElement('li');
+        const photoDiv = document.createElement('div');
+        const img = document.createElement('img');
+        const textDiv = document.createElement('div');
+
+
+        cardDiv.classList.add('card');
+        photoDiv.classList.add('photo');
+
+        let photo = 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-contact-512.png';
+        if(contactsArray[i].photo){
+            photo = contactsArray[i].photo
+        }
+        img.setAttribute('src', photo);
+
+        textDiv.classList.add('text');
+        textDiv.textContent = contactsArray[i].name;
+
+        ulElement.appendChild(li);
+        li.appendChild(cardDiv);
+        cardDiv.appendChild(photoDiv);
+        cardDiv.appendChild(textDiv);
+        photoDiv.appendChild(img);
+
+        i++;
+    }
+}
+
+function getDataFromJson() {
+    fetch('../api/characters.json')
+    .then(response => response.json())
+    .then(data => normalizeData(data))
+    .then(cleanData => renderContacts(cleanData))
+}
+
+function normalizeData(data) {
+    for(let i = 0; i < data.length; i++){
+        const newContact = {
+            name : data[i].name,
+            photo : data[i].image
+        }
+        contacts.push(newContact);
+    }
+    return contacts
+}
+
+function catchEvent(event){
+    const inputText = event?.target?.value.toLocaleLowerCase() || '';
+    console.log(inputText)
+}
+
+searchingInput.addEventListener('keyup', catchEvent);
+
+getDataFromJson();
